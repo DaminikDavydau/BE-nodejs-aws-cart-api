ff9395bc73fd1708302509600835f1c307fbbf19
DaminikDavydau <Dominikdavydov3d@gmail.com>
2023-07-10 23:34:21 +0300

    Initial commit

---
+++ test/be-nodejs-aws-cart-api.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as BeNodejsAwsCartApi from '../lib/be-nodejs-aws-cart-api-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/be-nodejs-aws-cart-api-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new BeNodejsAwsCartApi.BeNodejsAwsCartApiStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
