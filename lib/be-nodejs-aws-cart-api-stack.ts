2f3454cad489f92720f4ca10a63db37604cd8bbd
DaminikDavydau <Dominikdavydov3d@gmail.com>
2023-07-17 16:54:45 +0300

    feat: add once again all files to the repo

--- lib/be-nodejs-aws-cart-api-stack.ts
+++ lib/be-nodejs-aws-cart-api-stack.ts
@@ -1,16 +1,41 @@
 import * as cdk from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apiGateway from '@aws-cdk/aws-apigatewayv2-alpha';
+import { HttpLambdaIntegration } from '@aws-cdk/aws-apigatewayv2-integrations-alpha';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class BeNodejsAwsCartApiStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const handler = new lambda.Function(this, 'CartLambda', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset('dist'), // Assuming the compiled code is in the 'dist' directory
+      handler: 'main.handler',
+      environment: {
+        DB_HOST: 'nodejs-aws-db.c7ugxelvfu2k.eu-west-1.rds.amazonaws.com',
+        DB_PORT: '5432',
+        DB_USERNAME: 'defree',
+        DB_PASSWORD: 'One200Two',
+        DB_DATABASE: 'postgres',
+      },
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'BeNodejsAwsCartApiQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const apiCard = new apiGateway.HttpApi(this, 'CartApiHttp', {
+      corsPreflight: {
+        allowHeaders: ['*'],
+        allowOrigins: ['*'],
+        allowMethods: [apiGateway.CorsHttpMethod.ANY],
+      },
+    });
+
+    apiCard.addRoutes({
+      integration: new HttpLambdaIntegration(
+        'cartLambdaIntegration',
+        handler,
+      ),
+      path: '/{proxy+}',
+      methods: [apiGateway.HttpMethod.ANY],
+    });
   }
 }
